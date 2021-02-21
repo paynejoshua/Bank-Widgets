0e47714ce02a054a190584f55043cd72c53099f7
Josh Payne <paynejoshua1@gmail.com>
2021-02-20 21:08:37 -0800

    brought the base url in the apiRoutes folder back and proxy. Server is working again

--- client/src/utils/API/apiRoutes.js
+++ client/src/utils/API/apiRoutes.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/Bank-Widgets/client/src/utils"
+
 const API = {
     getRate: function(){
         return axios({
          method: "GET",   
-         url: "/API/getRate.php",
+         url: BASE_URL + "/API/getRate.php",
             
         })
       },
